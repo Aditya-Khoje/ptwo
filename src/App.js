95a2b4d55d4d04c54d56c11525bfe70a714eb7a8
Aditya-Khoje <adityakhoje1@gmail.com>
2024-02-02 23:20:26 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [objArray, setObjArray] = useState([]);
+
+  const func1 = () => {
+    setObjArray([
+      ...objArray,
+      {
+        Name: "Aditya",
+        Surname: "Khoje",
+        RollNo: 1,
+      },
+    ]);
+  };
+
+  console.log(objArray);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Aditya Khoje</h1>
+      <button onClick={func1}>Click</button>
+      {objArray.map((obj, index) => (
+        <div key={index}>
+          <h1>{`${obj.Name} ${obj.Surname} - Roll No: ${obj.RollNo}`}</h1>
+        </div>
+      ))}
+    </>
   );
 }
 
